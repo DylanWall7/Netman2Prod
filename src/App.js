a00d74016b5fde63553522c4fe3c04b57b02e16f
DylanWall7 <dylanw07.dw@gmail.com>
2026-08-14 10:30:25 -0500

    added check snipeit button to gear returns

--- src/App.js
+++ src/App.js
@@ -235,14 +235,14 @@ function App() {
                 </ProtectedRoute>
               }
             ></Route>
-            <Route
+            {/* <Route
               path="diagrams"
               element={
                 <ProtectedRoute allowedRoles={["Engineer"]}>
                   <SiteDiagramsView />
                 </ProtectedRoute>
               }
-            ></Route>
+            ></Route> */}
             {/* <Route path="dhcpmanager" element={<DHCPManager />} />
             <Route path="dhcpmanager/scope" element={<DHCPScopeDetail />} /> */}
             {/* <Route path="ogtemplate" element={<OgTemplate />} /> */}
